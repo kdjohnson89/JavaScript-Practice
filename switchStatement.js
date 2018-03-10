5773fa2d5d548041599ecf360e175d2e8644f7cd
Kayla Johnson <kdjohnson89@users.noreply.github.com>
2018-03-10 12:08:22 -0600

    Update switchStatement.js

--- switchStatement.js
+++ switchStatement.js
@@ -1,11 +1,6 @@
-/*
- * Programming Quiz: Back to School (3-9)
- */
 
-// change the value of `education` to test your code
 var education = "no high school diploma";
 
-// set the value of this based on a person's education
 var salary;
 
 switch (education) {
